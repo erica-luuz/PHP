62b650b3f7ff5bf21147e8e7d028b16f3373dbd9
Erica Giselle Francisco Luz <erica.nicollas_hbn@yahoo.com>
2024-10-02 22:27:33 -0300

    fazendo teste para iniciar o estudo PHP

---
+++ aula010/index_2.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <h1>HTML</h1>
+
+    <?php
+    echo 'PHP';
+    ?>
+
+    <p>
+        <?= 'PHP' ?>
+    </p>
+
+    <div>
+        <?php
+        print "Ola Mundo!";
+        ?>
+    </div>
+
+</body>
+
+</html>
