b7cc84fadbbcb7bac90be808a5276f008c1e7fde
Erica Giselle Francisco Luz <erica.nicollas_hbn@yahoo.com>
2024-10-03 21:23:27 -0300

    Comecando com variveis

---
+++ aula_013/index_1.php
@@ -0,0 +1,14 @@
+<?php
+
+    // Variaveis
+
+    $valor = 100;
+
+    // $Valor -> Nome da Variavel
+    // =  -> Operador de Atribuição
+    // 100  -> Valor da Variavel
+
+    // Nomes das Variaveis
+
+    $nome = "Erica";
+    $_idade = 42;
